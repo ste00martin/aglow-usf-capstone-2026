e6ea456983bf3fbb6ee7ae9b1f0ed1f86c242cfb
Peter Regalbuto <pcregalbuto@dons.usfca.edu>
2026-04-01 20:59:29 -0700

    First version of video moderation, some scrolling issues for now

--- expo-pytorch/app/(tabs)/videoupload.tsx
+++ expo-pytorch/app/(tabs)/videoupload.tsx
@@ -5,14 +5,28 @@ import { useContext } from "react";
 import * as ImagePicker from 'expo-image-picker';
 import { useVideoPlayer, VideoView } from 'expo-video';
 import * as VideoThumbnails from 'expo-video-thumbnails';
+import { useExecutorchModule, ScalarType } from "react-native-executorch";
+import type { TensorPtr } from "react-native-executorch";
+import { imageUriToTensor, postprocessBlazeFace, cropFace, imageUriToViTTensor, topFromLogits, allFromLogits } from "../../aipreprocessing"
 
 const VIDEO_MAX_DURATION = 30; // seconds
 const FRAME_INTERVAL = 5000; // milliseconds between frames to extract
+const NSFW_MODEL = require("../../assets/models/nsfw_model.pte");
+const NSFW_LABELS   = ['gore_bloodshed_violent', 'nudity_pornography', 'safe_normal'];
+
+const VIT_INPUT_SIZE = 224;
+
+type ImageResult = {
+    uri: string;
+    nsfw: { label: string; score: number };
+};
 
 export default function VideoUploadScreen() {
     // some code here.
     const [video, setVideo] = useState<string>("");
     const [frames, setFrames] = useState<string[]>([]); // array of asset uris for frames from video
+    const [thumbnails, setThumbnails] = useState<ImageResult[]>([]);
+    const nsfwModel = useExecutorchModule({ modelSource: NSFW_MODEL });
 
     const pickVideo = async () => {
         const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -31,12 +45,34 @@ export default function VideoUploadScreen() {
         
         if (!result.canceled) { // if we get something..
             const selectedUri = result.assets[0].uri;
+
+            const thumbnails: ImageResult[] = [];
+
             setVideo(result.assets[0].uri); // always chooses the first video
             for (let time = 0; time < VIDEO_MAX_DURATION * 1000; time += FRAME_INTERVAL) {
                 const { uri: thumbnail } = await VideoThumbnails.getThumbnailAsync(selectedUri, { time: time });
                 setFrames(prev => [...prev, thumbnail]);
+                
                 console.log(`Extracted frame at ${time}ms: ${thumbnail}`);
+
+                const nonCroppedVitTensor = await imageUriToViTTensor(thumbnail);
+                const nonCroppedVitTensorPtr: TensorPtr = {
+                    dataPtr: nonCroppedVitTensor,
+                    sizes: [1, 3, VIT_INPUT_SIZE, VIT_INPUT_SIZE],
+                    scalarType: ScalarType.FLOAT,
+                };
+
+                //const nsfwOutputs = await Promise.all(nsfwModel.forward([nonCroppedVitTensorPtr]);)
+                const nsfwOutputs = await nsfwModel.forward([nonCroppedVitTensorPtr]);
+
+                const nsfwLogits   = new Float32Array(nsfwOutputs[0].dataPtr as ArrayBuffer);
+
+                thumbnails.push({
+                    uri: thumbnail,
+                    nsfw: topFromLogits(nsfwLogits, NSFW_LABELS),
+                })
             }
+            setThumbnails([...thumbnails])
         }
     };
     const player = useVideoPlayer(
@@ -44,17 +80,31 @@ export default function VideoUploadScreen() {
     );
 
     return (
-        <View style={styles.container}>
-            <Pressable style={styles.buttonContainer} onPress={pickVideo}>
-                <Text style={styles.button}>Upload a Video.</Text>
-            </Pressable>                
-            <VideoView
-                player={player}
-                style={styles.video}
-                nativeControls        // show native play/pause controls
-                contentFit="contain" // keeps aspect ratio
-            />
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+    <Pressable style={styles.buttonContainer} onPress={pickVideo}>
+      <Text style={styles.button}>Upload a Video.</Text>
+    </Pressable>                
+
+    <VideoView
+      player={player}
+      style={styles.video}
+      nativeControls
+      contentFit="contain"
+    />
+
+    {thumbnails.map((item, index) => (
+      <View key={index} style={{ marginVertical: 12, alignItems: 'center' }}>
+        <Image
+          source={{ uri: item.uri }}
+          style={{ width: 200, height: 120 }}
+        />
+
+        <Text>
+          NSFW: {item.nsfw.label} ({(item.nsfw.score * 100).toFixed(1)}%)
+        </Text>
+      </View>
+    ))}
+    </ScrollView>
     );
 }
 
